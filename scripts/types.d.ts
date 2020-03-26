0141abc137f4962b249f34390d3c102a713d9c5e
jinc.cjc <jinc.cjc@alibaba-inc.com>
2020-03-26 12:07:40 +0800

    fix(types): export types

--- scripts/types.d.ts
+++ scripts/types.d.ts
@@ -1,40 +1,24 @@
-export type Base = { [key: string]: object }
-export type ComponentDetail<T extends Base> = { [P in keyof T]?: T[P] }
-export type ComponentData<T> = { [P in keyof T]?: FiledData<T[P]> }
-export type FiledData<T> = {
-  fields: T
+type CommonProps = {
+  /**
+   * this is just name of the library kebab-cased
+   */
+  libraryName: string
+  description: string
+  keywords: string[]
+  useVSCode: boolean
+  username: string
+  usermail: string
+  githubName: string
 }
 
-export interface IData {
-  id?: string
-  status?: number
-  resource?: object
-  type?: string
-  [key: string]: any
-}
-
-export interface IComponentRenderDO<AllComponents> {
-  id: keyof AllComponents
-  n: string
-  d: object
-  childrens: (IComponentRenderDO<AllComponents>)[] | []
-}
-
-export type Structure<T> = { [P in keyof T]: Array<keyof T> }
-export interface OBS_Schema<
-  Components extends Base,
-  AllComponents extends Base
-> {
-  data: ComponentData<AllComponents>
-  endpoint?: {
-    mode?: string
-    pageCode?: string
-    protocolVersion: string
-  }
-  hierarchy: {
-    component?: Array<keyof AllComponents>
-    componentDetail: ComponentDetail<Components>
-    root: keyof AllComponents
-    structure: Structure<AllComponents>
-  }
-}
+export type LibConfig = {
+  /**
+   * if scope is provided this property is the published npm name `@scope/libName`
+   */
+  packageName: string
+  version?: string
+} & CommonProps
+export type PromptAnswers = {
+  useScope: boolean
+  scope: string
+} & CommonProps
